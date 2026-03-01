ce601c191b809b832d27b1ebe3f31e6180ee7ebe
gabrielvitorbm-br <gabrielvitorbm@gmail.com>
2026-03-01 11:12:39 -0300

    exercicio 18 feito

--- Lista_2/exercicio18.php
+++ Lista_2/exercicio18.php
@@ -3,21 +3,21 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Calculando juros simples</title>
+<title>Calculando o montante com juros composto</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body> 
 <div class="container py-3">
-<h1>Calculando juros simples</h1>
+<h1>Calculando o montante com juros composto</h1>
 <form method="post">
 <div class="mb-3">
-              <label for="capital" class="form-label">insira o capital com "." no lugar de ","</label>
+              <label for="capital" class="form-label">Informe a capital com "." no lugar de ","</label>
               <input type="number" id="capital" name="capital" class="form-control" step="0.01" required="">
             </div><div class="mb-3">
-              <label for="juros" class="form-label">taxa de juros em %  com "." no lugar de ","</label>
+              <label for="juros" class="form-label">Informe a taxa de juros com "." no lugar de ","</label>
               <input type="number" id="juros" name="juros" class="form-control"  step="0.01"required="">
             </div><div class="mb-3">
-              <label for="periodo" class="form-label">informe o periodo</label>
+              <label for="periodo" class="form-label">Informe o periodo</label>
               <input type="number" id="periodo" name="periodo" class="form-control" required="">
             </div>
 <button type="submit" class="btn btn-primary">Enviar</button>
@@ -27,10 +27,9 @@
         $capital = $_POST["capital"];
         $juros = $_POST["juros"];
         $periodo = $_POST["periodo"];
-        $jurossimples = $capital * ($juros/100) * $periodo;
-        $resultado = $jurossimples + $capital;
+        $juroscomposto = $capital * (1+($juros/100)) ** $periodo;
 
-        echo "a taxa de juros vai ser $jurossimples e o total que vai ter é: $resultado";
+        echo "O resultado vai ser: $juroscomposto";
     }
 
 ?>
