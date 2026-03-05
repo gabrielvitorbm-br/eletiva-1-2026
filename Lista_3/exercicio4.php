9d6ee963cee11a09cf76514462f4b4576864b085
gabrielvitorbm-br <gabrielvitorbm@gmail.com>
2026-03-05 08:52:46 -0300

    exercicio 4

---
+++ Lista_3/exercicio4.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Ondem crescente </title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Calculando desconto </h1>
+<form method="post">
+<div class="mb-3">
+              <label for="valor" class="form-label">Informe o valor</label>
+              <input type="number" id="valor" name="valor" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $n1 = $_POST["valor"];
+        
+
+        if( $n1 > 100)
+            $n1 = $n1 - ($n1 * (15 / 100));
+        
+        echo "<p> O valor vai ser: $n1</p>";
+    }   
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
