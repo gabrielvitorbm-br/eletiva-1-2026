974de4c54977fed7d40070839a4820ba5582a982
gabrielvitorbm-br <gabrielvitorbm@gmail.com>
2026-03-15 09:38:44 -0300

    exercicio 5

---
+++ lista_4/exercicio5.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>raiz quadrada</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>raiz quadrada</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="numero" class="form-label">informe um numero</label>
+              <input type="number" id="numero" name="numero" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+        $numero = $_POST["numero"];
+        $raiz = sqrt($numero);
+
+        echo "A raiz quadrada de $numero é $raiz";
+
+    }
+
+
+?>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
